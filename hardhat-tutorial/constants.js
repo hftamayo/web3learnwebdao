29c08c0ec27de8f2f8daabf29246e4c9204bebea
hftamayo <hftamayo@gmail.com>
2023-01-10 19:31:14 -0600

    v1 before testing and deployment

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xcDA084dF8CA647603E7b32d0b82F499657Ff66E8";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
